9b56c845b6714e1b1f6baadaeff6afa225c14e93
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2024-05-24 09:38:34 +0200

    Add markers.

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,11 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="map"
 export default class extends Controller {
+  static values = {
+    coordinates: Array
+  }
+
   connect() {
-    var map = L.map('map').setView([59.91087115068967, 10.729828757277915], 14);
+    var map = L.map('map').setView(
+      [59.92472143605738, 10.73411569091055], // a center-coordinate that is able to display all markers.
+      14
+    );
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    this.coordinatesValue.forEach((coordinate) => {
+      L.marker(coordinate).addTo(map);
+    })
   }
 }
