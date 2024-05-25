0c6bee33b30818557bc833c89ca254faf4ba6c2c
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2024-05-25 11:06:37 +0200

    Add more tests and fix the naming of one.

--- test/controllers/maps_controller_test.rb
+++ test/controllers/maps_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 require "json"
 
 class MapsControllerTest < ActionDispatch::IntegrationTest
-  test "the truth" do
+  test "stands rest api" do
     get "/stands"
 
     assert_response :success
@@ -15,5 +15,11 @@ class MapsControllerTest < ActionDispatch::IntegrationTest
     assert_instance_of Integer, first_stand['num_bikes_available']
     assert_instance_of Float, first_stand['lat']
     assert_instance_of Float, first_stand['lon']
+  end
+
+  test "can see root page" do
+    get root_url
+
+    assert_select "h1", "Bysykkelstativer"
   end
 end
